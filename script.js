d3fd42e3570b44e519b462beb8fe21ca22c53cad
franckkoukougnon <franckkoukougnon@gmail.com>
2020-05-27 19:57:07 +0200

    revisions string

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert ("Mael Ethan");
+var header = document.getElementById('header');
+header.innerHTML = "Revisions JS";
+
+var firstName = "il a dit : \"C'est une simple phrase\"";
+console.log(firstName);
+
+
+var firstName2 = 3 ;
+console.log(firstName2);
