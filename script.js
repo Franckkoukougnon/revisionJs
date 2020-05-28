80be2fcec1f1a66cc893fc8d157f9262aba11bcd
franckkoukougnon <franckkoukougnon@gmail.com>
2020-05-28 17:34:13 +0200

    operation ternaire

--- script.js
+++ script.js
@@ -1,33 +1,6 @@
-var header = document.getElementById('header');
-header.innerHTML = "Revisions JS"; //  cette annotation est equivalente à document.getElementById.innerHTML
+// operation ternaire 
 
+var age = 43;
 
-
-
-
-
-// Array
-var color = ["vert", "rouge", "violet", 5];
-
-color[6]= "bleu";
-
-console.log(color);
-
-//Object
-
-var identité = {
-    firsName : "Koukougnon",
-    lastName : "Mael Ethan",
-    age: 3 ,
-    sexe : "M",
-
-}
-
-identité.town = "Bordeaux";
-
-identité.lastName = "Mael Ethan Gnagra";
-
-console.log (identité);
-
-
+ age >= 18 ? console.log (` a ${age} ans, vous etes majeur !!!`) : console.log (`a ${age} ans, vous etes encore mineur !!!`);
 
