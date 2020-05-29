64ed4ebb9f92e4866d20fc0597cca6bf3598fbba
franckkoukougnon <franckkoukougnon@gmail.com>
2020-05-29 18:03:23 +0200

    boucle For

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
+// Boucle For
 
+for (var i = 1; i < 10 ; i++){
 
-var a = 10;
-do {
-     console.log(`la valeur de la variable est: ${a}`);
-     a++;
- } while (a<10)
-
- console.log('la boucle est finie !!!');
+    if (i == 5){
+        console.log (`Ici la valeur de i est ${i}, donc on fait une pause !!!`)
+        break;
+    }
+    console.log(`la valeur de la variable i est de ${i}`);
+}
+    console.log('La boucle est finie !!!');
